2dbb76d1da9a41a2f1fae7a82e59999fdfebba33
CptBang <muashims@gmail.com>
2020-02-20 11:28:49 -0800

    parsing complete

--- Moo_parse.go
+++ Moo_parse.go
@@ -8,11 +8,14 @@ import (
 	"strings"
 )
 
-// func createLibrary(lib1 string, lib2 string) Library struct {
+func parseBookIds(secondLine []string) []int {
+	bookIds := make([]int, len(secondLine))
+	for i := 0; i < len(secondLine); i++ {
+		bookIds[i], _ = strconv.Atoi(secondLine[i])
+	}
 
-// 	NewLibrary()
-
-// }
+	return bookIds
+}
 
 func scanText(file string) []string {
 	f, _ := os.Open(file)
@@ -34,25 +37,33 @@ func parseFile(file string) {
 	numLibraries, _ := strconv.Atoi(line1[1]) // total number of libraries
 	numDays, _ := strconv.Atoi(line1[2]) // total number of days for scanning
 
+	// create array of book scores
 	line2 := strings.Fields(result[1])
-	// bookScores :=
+	bookScores := make([]int, numBooks)
+	for i := 0; i < numBooks; i++ {
+		bookScores[i], _ = strconv.Atoi(line2[i])
+	}
 
-	// create all libraries and put into array
-	// var libraries [numLibraries]Library
-	// for i := 0; i < len(libraries); i++{
-	// 	for j := 2; j <
-	// 	createLibrary()
-	// }
+	//create all libraries and put into array
+	libraries := make([]Library, numLibraries)
+	for i := 0; i < numLibraries; i++{
+		for j := 2; j < numLibraries * 2; j = j+2 {
+			firstLine := strings.Fields(result[j])
 
-	fmt.Printf("%s", line1)
-	fmt.Printf("%s", line2)
-	fmt.Printf("%d", numBooks)
-	fmt.Printf("%d", numLibraries)
-	fmt.Printf("%d", numDays)
+			secondLine := strings.Fields(result[j+1])
 
-	// for i := 0; i < K; i++ {
-	// 	intPizzas[i], _ = strconv.Atoi(strPizzas[i])
-	// }
+			bookCount, _ := strconv.Atoi(firstLine[0])
+			signupLen, _ := strconv.Atoi(firstLine[1])
+			shipRate, _ := strconv.Atoi(firstLine[2])
+			libraries[i] = makeLibrary(bookCount, signupLen, shipRate, parseBookIds(secondLine))
+		}
+	}
+
+
+	fmt.Printf("%d\n", numBooks)
+	fmt.Printf("%d\n", numLibraries)
+	fmt.Printf("%d\n", numDays)
+
 }
 
 // array
